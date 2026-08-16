ad1909d3949fad57bea82e51f1042384e997b5ad
Prakhar <prakharshakya28@gmail.com>
2026-08-17 01:20:49 +0530

    Refactor navigation props in App component

--- login-register/src/App.jsx
+++ login-register/src/App.jsx
@@ -8,56 +8,34 @@ import Home from "./components/Home";
 import "./App.css";
 
 function App() {
-
     const [page, setPage] = useState("login");
-
 
     return (
         <>
-
             {page === "login" && (
                 <Login
-                    goToRegister={() =>
-                        setPage("register")
-                    }
-
-                    goToForgot={() =>
-                        setPage("forgot")
-                    }
-
-                    goToHome={() =>
-                        setPage("home")
-                    }
+                    goToRegister={() => setPage("register")}
+                    goToForgot={() => setPage("forgot")}
+                    goToHome={() => setPage("home")}
                 />
             )}
-
 
             {page === "register" && (
                 <Register
-                    goToLogin={() =>
-                        setPage("login")
-                    }
+                    goToLogin={() => setPage("login")}
                 />
             )}
-
 
             {page === "forgot" && (
                 <ForgotPassword
-                    goToLogin={() =>
-                        setPage("login")
-                    }
-
-                    goToRegister={() =>
-                        setPage("register")
-                    }
+                    goToLogin={() => setPage("login")}
+                    goToRegister={() => setPage("register")}
                 />
             )}
-
 
             {page === "home" && (
                 <Home />
             )}
-
         </>
     );
 }
